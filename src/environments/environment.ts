16974b04f0f0a9112c6a8fd6eae71c9e59ef97cd
Rosarial <rosarialdeveloper@gmail.com>
2024-09-24 12:04:39 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   // https://galaxyteste.online/apis/apis/clientes/
   // https://dashboard.render.com/web/srv-cpl4rgi0si5c738fu1qg/logs
   // baseUrl: 'https://apirosarial.rmscorretorachat.com.br',
-  baseUrl: 'http://rosarialapp.online',
+  baseUrl: 'https://rosarialapp.online',
   apis: '/api',
   production: false,
   distancePermited: 500, // 1km 1000m
