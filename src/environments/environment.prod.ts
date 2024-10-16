70fe2ef21e7f3f3293d4cf5cdc2aceac2cd6436d
Rosarial <rosarialdeveloper@gmail.com>
2024-10-16 16:24:14 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
   baseUrl: 'https://rosarialapp.online',
   apis: '/api',
   production: true,
-  distancePermited: 10000, // 1km 1000m
-  distanceStoresPermitted: 20000, // 10km 1000m
+  distancePermited: 100000, // 1km 1000m
+  distanceStoresPermitted: 200000, // 10km 1000m
   appInfo: {
     name: 'Prom. Rosarial',
     bundledId: 'app.promotores-rosarial',
